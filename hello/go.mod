998ed3c81972ec965907fa49aaf0905e37e9a512
Saichethanm <86402141+Saichethanm@users.noreply.github.com>
2021-08-09 13:35:48 +0530

    Go getting started

--- hello/go.mod
+++ hello/go.mod
@@ -1,10 +1,5 @@
-module example.com/hello
+module example.com/user/hello
 
 go 1.16
 
-require (
-	example.com/greetings v0.0.0-00010101000000-000000000000
-	rsc.io/quote v1.5.2
-)
-
-replace example.com/greetings => ../greetings
+require github.com/google/go-cmp v0.5.6
--- hello/hello2.go
+++ hello/hello2.go
@@ -1,28 +1,12 @@
 package main
-
-import (
-    "fmt"
-    "log"
-
-    "example.com/greetings"
+import(
+	"fmt"
+	
+	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
-func main() {
-    // Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
-
-    // A slice of names.
-    names := []string{"Gladys", "Samantha", "Darrin"}
-
-    // Request greeting messages for the names.
-    messages, err := greetings.Hellos(names)
-    if err != nil {
-        log.Fatal(err)
-    }
-    // If no error was returned, print the returned map of
-    // messages to the console.
-    fmt.Println(messages)
+func main(){
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
